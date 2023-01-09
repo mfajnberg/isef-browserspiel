06a9e650323b3a4adc4df1cc4d2c231554d4aa27
Maximilian Fajnberg <max_fajnberg@gmx.de>
2023-01-09 15:17:20 +0100

    Added world manager class

---
+++ backend-solution/web-api/Services/DemoWorldManager.cs
@@ -0,0 +1,23 @@
+﻿using web_api.GameModel;
+using web_api.GameModel.Sites;
+
+namespace web_api.Services
+{
+    public class DemoWorldManager
+    {
+        private int Id { get; set; }
+        private List<HexTile> _worldMap { get; set; } = new List<HexTile>();
+        private List<SiteBase> _sites { get; set; } = new List<SiteBase>();
+        private bool _isInitialized { get; set; }
+
+        public DemoWorldManager() { }
+
+        public void LoadWorldMap() { }
+        public void InitObstacles() { }
+        public void InitSettlements() { }
+        public void InitPerils() { }
+        public void PlaceTreasures() { }
+        public void PlaceGuards() { }
+
+    }
+}
