4aa12b26a5a265f977360b6f08b01aa7edd54d0f
Maximilian Fajnberg <max_fajnberg@gmx.de>
2023-01-12 19:37:38 +0100

    moved world manager into game model; admin controller draft

---
+++ backend-solution/web-api/GameModel/DemoWorldManager.cs
@@ -0,0 +1,16 @@
+﻿using web_api.GameModel.Sites;
+
+namespace web_api.GameModel
+{
+    public class DemoWorldManager
+    {
+        private List<SiteBase> _obstacles { get; set; } = new List<SiteBase>();
+        private List<SiteBase> _settlements { get; set; } = new List<SiteBase>();
+        private List<SiteBase> _perils { get; set; } = new List<SiteBase>();
+
+        public void InitObstacles(DataContext context) { }
+        public void InitSettlements(DataContext context) { }
+        public void InitPerils(DataContext context) { }
+
+    }
+}
