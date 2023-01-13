034b51ad14d236b9543be106e484607bc3348214
Maximilian Fajnberg <max_fajnberg@gmx.de>
2023-01-13 21:12:09 +0100

    made asset streaming more flexible; drafted demo world gen data model

---
+++ backend-solution/web-api/HexTileDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using web_api.GameModel;
+
+namespace web_api
+{
+    public class HexTileDto
+    {
+        public HexVector Vector { get; set; }
+        public int SiteBaseId { get; set; }
+        public HexTileDto(int axialQ, int axialR, int siteBaseId)
+        {
+            Vector = new HexVector(axialQ, axialR);
+            SiteBaseId = siteBaseId;
+        }
+    }
+}
